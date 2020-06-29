417875924e7f86f4f6e11617c8af209b9d4e3fee
enzo <enzo.seuve@laposte.net>
2020-06-29 10:33:39 +0200

    programme d'allumage

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Favoris from './Components/Favoris';
+import Programme from './Components/Programme';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/Favoris" component={Favoris} />
+        <Route path="/Programme" component={Programme} />
       </Switch>
     </BrowserRouter>
   );
