2baed20ce3277a5fc1339e7bf0998317153d9217
Emeline <mariemeline.paus@gmail.com>
2020-07-17 15:08:11 +0200

    dotenv

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
